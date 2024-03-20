42590082b04ac83a1556c57fcc7d41455b63439f
Jeonghwan Park <pjhisgod@naver.com>
2024-03-20 16:57:27 +0900

    mariadb

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     ConfigModule.forRoot({ envFilePath: ['.env'], isGlobal: true }),
     TypeOrmModule.forRoot({
-      type: 'postgres',
+      type: 'mariadb',
       host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
@@ -17,6 +17,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoLoadEntities: true, //자동으로 엔티티 load
       synchronize: true, //개발 중에만 쓰고, 실제 프로덕트에선 끄기
       logging: true, // 자동으로 쿼리문과 에러 로깅,
+      timezone: 'Asia/Seoul',
     }),
   ],
   controllers: [AppController],
